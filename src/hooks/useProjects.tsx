a2113539b7ddd55b0b5b25833b2881673fbf6ae6
gavsidhu <gavinjeet.sidhu@gmail.com>
2022-12-24 18:41:34 -0800

    feat: created use projects hook to access projects for /projects and /

---
+++ src/hooks/useProjects.tsx
@@ -0,0 +1,71 @@
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+
+import { db } from '@/lib/firebaseClient';
+import useAuth from '@/hooks/useAuth';
+
+import { Project } from '@/types';
+
+interface IProjects {
+  projectLoading: boolean | null;
+  projects: Project[] | null;
+}
+
+const ProjectContext = createContext<IProjects>({
+  projectLoading: false,
+  projects: null,
+});
+
+interface ProjectProviderProps {
+  children: React.ReactNode;
+}
+export const ProjectProvider = ({ children }: ProjectProviderProps) => {
+  const { user } = useAuth();
+  const [projectLoading, setProjectLoading] = useState<boolean>(true);
+  const [projects, setProjects] = useState<Project[] | null>(null);
+  useEffect(() => {
+    const q = query(
+      collection(db, 'projects'),
+      where('uid', '==', user?.uid),
+      orderBy('lastOpened', 'desc')
+    );
+    return onSnapshot(q, (snapshot) => {
+      const arr: Project[] = [];
+      snapshot.docs.map((doc) => {
+        const obj = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        arr.push(obj as Project);
+      });
+      setProjects(arr);
+      setProjectLoading(false);
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [db]);
+
+  const memoedValue = useMemo(
+    () => ({
+      projectLoading,
+      projects,
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [projectLoading, projects]
+  );
+  return (
+    <ProjectContext.Provider value={memoedValue}>
+      {children}
+    </ProjectContext.Provider>
+  );
+};
+
+export default function useProjects() {
+  return useContext(ProjectContext);
+}
