7a506cf3056f1e12d23b3bc950d3976aeb9a9b31
gavsidhu <gavinjeet.sidhu@gmail.com>
2022-12-24 18:12:38 -0800

    feat: created empty state and table component for home page

---
+++ src/components/home/EmptyState.tsx
@@ -0,0 +1,36 @@
+import { Card } from 'flowbite-react';
+import React, { useState } from 'react';
+import { HiFolder, HiPlus } from 'react-icons/hi2';
+
+import Button from '@/components/buttons/Button';
+import NewProjectModal from '@/components/projects/NewProjectModal';
+
+const EmptyState = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = (value: boolean) => {
+    setShowModal(value);
+  };
+  return (
+    <Card className='py-6'>
+      <div className='text-center'>
+        <HiFolder className=' mx-auto h-12 w-12 text-gray-400' />
+        <h3 className='mt-2 text-sm font-medium text-gray-900'>No projects</h3>
+        <p className='mt-3 text-sm text-gray-500'>
+          Get started by creating a new project.
+        </p>
+        <div className='mt-6'>
+          <NewProjectModal
+            handleShowModal={handleShowModal}
+            isOpen={showModal}
+          />
+          <Button onClick={() => handleShowModal(true)}>
+            <HiPlus className='-ml-1 mr-2 h-5 w-5' aria-hidden='true' />
+            New Project
+          </Button>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default EmptyState;
---
+++ src/components/home/ProjectsTable.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+import React from 'react';
+import { HiArrowRight } from 'react-icons/hi2';
+
+import { findDaysDifferent } from '@/utils/findDaysDifferent';
+
+import { Project } from '@/types';
+
+type Props = {
+  projects: Project[];
+};
+const ProjectsTable = ({ projects }: Props) => {
+  return (
+    <>
+      <div className='flex flex-row justify-between border-b-2'>
+        <div className='px-1 py-2'>
+          <h2 className='text-2xl font-semibold'>Recent projects</h2>
+        </div>
+        <Link
+          href='/projects'
+          className='flex flex-row items-center space-x-2 px-1 py-2'
+        >
+          <p>See all projects</p>
+          <HiArrowRight className='h-4 w-4' />
+        </Link>
+      </div>
+      <div className='py-6'>
+        <div className='overflow-hidden bg-white shadow sm:rounded-md'>
+          <ul role='list' className='divide-y divide-gray-200'>
+            {projects.map((project) => (
+              <li key={project.id} id={project.id}>
+                <Link
+                  href={`/projects/${project.id}`}
+                  className='block hover:bg-gray-50'
+                >
+                  <div className='flex items-center justify-between px-4 py-4 sm:px-6'>
+                    <div className='flex items-center justify-between'>
+                      <h3 className='text-primary truncate text-lg font-medium'>
+                        {project.projectName}
+                      </h3>
+                    </div>
+                    <div className='mt-2 sm:flex sm:justify-end'>
+                      <div className='mt-2 flex items-center text-sm text-gray-500 sm:mt-0'>
+                        <p>
+                          Last opened {findDaysDifferent(project.lastOpened)}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProjectsTable;
