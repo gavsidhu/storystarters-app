cc185428beda45e6dcd797e9159845dca10f3cea
gavsidhu <gavinjeet.sidhu@gmail.com>
2022-12-24 18:56:45 -0800

    refactor: added types to one file to follow DRY principle

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+import { Content } from '@tiptap/react';
+export interface Project {
+  id: string;
+  userId: string;
+  projectName: string;
+  projectDescription: string;
+  wordCountGoal: string | number;
+  wordCount: string | number;
+  dateCreated: string | Date;
+  lastOpened: string | Date;
+}
+
+export type CustomData = {
+  fileType: string;
+  fileSize: string;
+  content: Content;
+} | null;
+
+export type DocumentModel = {
+  id?: string | number;
+  projectId: string;
+  userId: string;
+  node: {
+    parent: string | number;
+    droppable: boolean;
+    text: string;
+    data: {
+      content: string;
+      fileSize: string;
+      fileType: string;
+    };
+  };
+};
