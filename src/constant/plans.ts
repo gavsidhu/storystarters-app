4735b6ad6d41b42dae6f49a7624143d949b75959
gavsidhu <gavinjeet.sidhu@gmail.com>
2022-12-29 20:29:06 -0800

    fix: added test price id's

--- src/constant/plans.ts
+++ src/constant/plans.ts
@@ -1,5 +1,5 @@
 export const plans = {
-  tier1: '',
-  tier2: '',
-  tier3: 'price_1MFRYsDvnEqvQtxz7C3VaJlG',
+  tier1: 'price_1LTgqCDvnEqvQtxzF2wMxeoc',
+  tier2: 'price_1LTgrIDvnEqvQtxzUOPT53vC',
+  tier3: 'price_1LTgs3DvnEqvQtxzQi8FXTV9',
 };
