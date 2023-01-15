b3fe816b15f6b925e0bb1ef6bd167e20a0a6c6fc
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-01-15 11:20:37 -0800

    build: added constants for popular tools and resources

---
+++ src/constant/popularResources.ts
@@ -0,0 +1,22 @@
+const popularResources = [
+  {
+    id: '1',
+    title: 'Prompt library',
+    description: 'Hundreds of writing prompts',
+    href: '#',
+  },
+  {
+    id: '2',
+    title: "Hero's journey template",
+    description: "Template of hero's journey story structure",
+    href: '#',
+  },
+  {
+    id: '3',
+    title: 'Three act structure template',
+    description: 'Template of the three act story structure',
+    href: '#',
+  },
+];
+
+export default popularResources;
---
+++ src/constant/popularTools.ts
@@ -0,0 +1,22 @@
+const popularTools = [
+  {
+    id: '1',
+    title: 'Story idea generator',
+    description: 'generates story ideas',
+    href: '/tools/story-idea-generator',
+  },
+  {
+    id: '2',
+    title: 'Character Creator',
+    description: 'Create memorable character',
+    href: '#',
+  },
+  {
+    id: '3',
+    title: 'Scene builder',
+    description: 'builds scenses',
+    href: '#',
+  },
+];
+
+export default popularTools;
