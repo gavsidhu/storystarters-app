013ba9eded4a63162266e6134f20218daa2b448e
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-01-21 15:39:11 -0800

    feat: added character creator feature

---
+++ src/pages/api/generate/character-creator.ts
@@ -0,0 +1,182 @@
+import axios from 'axios';
+import { doc, getDoc } from 'firebase/firestore';
+import { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
+import { NextApiRequest, NextApiResponse } from 'next';
+import {
+  Configuration,
+  CreateCompletionResponseUsage,
+  OpenAIApi,
+} from 'openai';
+
+import { applyMiddleware, getRateLimitMiddlewares } from '@/lib/applyRateLimit';
+import { admin } from '@/lib/firebaseAdmin';
+import { db } from '@/lib/firebaseClient';
+
+import { plans } from '@/constant/plans';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const middlewares = getRateLimitMiddlewares({ limit: 30 }).map(applyMiddleware);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    try {
+      await Promise.all(middlewares.map((middleware) => middleware(req, res)));
+    } catch {
+      return res.status(429).send('Too Many Requests. Please try again later');
+    }
+
+    try {
+      const idToken = req.headers.authorization;
+      const uid = req.body.uid;
+      const genre = req.body.genre;
+      const role = req.body.role;
+      const plot = req.body.plot;
+
+      if (!idToken) {
+        return res.status(401).send('Unauthorized 1');
+      }
+      let user: DecodedIdToken;
+      try {
+        user = await admin.auth().verifyIdToken(idToken as string);
+      } catch (error) {
+        return res.status(401).send('Unauthorized 2');
+      }
+
+      if (user.uid != uid) {
+        return res.status(401).send('Unauthorized 3');
+      }
+
+      const docSnap = (await getDoc(doc(db, 'users', uid))).data();
+
+      if (!docSnap) {
+        throw new Error('User does not exist');
+      }
+
+      let isSubscribed: boolean;
+
+      if (
+        docSnap.subscription.planId == plans.tier1 ||
+        docSnap.subscription.planId == plans.tier2 ||
+        docSnap.subscription.planId == plans.tier3
+      ) {
+        isSubscribed = true;
+      } else {
+        isSubscribed = false;
+      }
+
+      if (!isSubscribed) {
+        return res.status(401).send('Unauthorized 4');
+      }
+
+      const currentTokens = docSnap.subscription.tokens;
+      const estimatedTokens = (200 / 3.8 + 10) * 2;
+
+      if (currentTokens <= estimatedTokens) {
+        return res.status(400).send('You have exceeded your monthly limit');
+      }
+      const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: generatePlot(plot, genre, role),
+        temperature: 0.7,
+        max_tokens: 759,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        stop: ['==='],
+        user: 'testing',
+      });
+
+      const moderationRes = await axios.post(
+        'https://api.openai.com/v1/moderations',
+        {
+          input: response.data.choices[0].text,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          },
+        }
+      );
+      if (moderationRes.data.results.flagged === true) {
+        return res
+          .status(400)
+          .send(
+            'AI generated prohibited content. Please try again or try a different selection. Monthly word limit not affected.'
+          );
+      }
+      const updatedTokens =
+        currentTokens -
+        (response.data.usage as CreateCompletionResponseUsage).total_tokens;
+      await admin
+        .firestore()
+        .collection('users')
+        .doc(uid)
+        .update({ 'subscription.tokens': updatedTokens });
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ msg: 'Unexpected error', error: error });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
+
+function generatePlot(plot: string, genre: string, role: string) {
+  return `create a character based on the following information:
+plot: The circumstances surrounding the death of crime novelist Harlan Thrombey are mysterious, but there's one thing that renowned Detective Benoit Blanc knows for sure -- everyone in the wildly dysfunctional Thrombey family is a suspect. Now, Blanc must sift through a web of lies and red herrings to uncover the truth.
+genre: mystery
+role: tertiary character
+~~~
+Name: Marie Thrombey
+
+Age: 45
+
+Appearance: Marie is an attractive woman in her mid-forties with shoulder-length curly brown hair, bright green eyes and light freckles on her cheeks. She is usually dressed in a smart business outfit with a pencil skirt, a fitted blouse and a blazer.
+
+Background: Marie is the youngest daughter of Harlan Thrombey and the only sibling to remain close with him until his untimely death. She works as a public relations specialist for a large technology company, but she always finds time to visit her father and catch up on his latest projects.
+
+Personality: Marie is a bubbly and outgoing person who loves to talk and connect with people. She is highly intelligent and well-spoken, with a sharp wit and an eye for detail. She is also fiercely loyal to her family, and her father's death has left her shaken and determined to uncover the truth.
+
+Role: Marie is a tertiary character in the mystery plot. She is an important part of the Thrombey family, and Detective Benoit Blanc will rely on her to provide an insider's perspective and help him uncover the truth.
+
+Motivation: Marie will do whatever it takes to get to the bottom of her father's death, and she is willing to put her own reputation and safety on the line to get justice for her father.
+
+Occupation: Marie is a public relations specialist for a large technology company.
+
+Flaws: Marie can be too trusting, which can get her into trouble when she's looking into her father's death. She also has a tendency to be too outspoken and direct, which can rub people the wrong way.
+===
+create a character based on the following information:
+plot: The circumstances surrounding the death of crime novelist Harlan Thrombey are mysterious, but there's one thing that renowned Detective Benoit Blanc knows for sure -- everyone in the wildly dysfunctional Thrombey family is a suspect. Now, Blanc must sift through a web of lies and red herrings to uncover the truth.
+genre: mystery
+role: protaganist
+~~~
+Name: Benoit Blanc
+
+Age: 55
+
+Appearance: Benoit is an imposing figure with a tall, muscular build, square jaw and piercing blue eyes. He usually wears a well-tailored suit and a trench coat, and he keeps his hair cut short. 
+
+Background: Benoit is a renowned detective who has solved numerous cases in his long career. He has a reputation for being able to get to the bottom of even the most complex cases, and his services have been requested by the Thrombey family to investigate the mysterious death of Harlan Thrombey.
+
+Personality: Benoit is a no-nonsense, logical thinker who pays close attention to details. He is highly observant and analytical, and he has an uncanny ability to see through lies and uncover the truth. He is also a highly moral and ethical person who always puts the truth first.
+
+Role: Benoit is the protagonist of the mystery plot. He is tasked with uncovering the truth behind Harlan Thrombey's death, and he will have to rely on his detective skills and intuition to do so. 
+
+Motivation: Benoit is driven by his sense of justice and his desire to uncover the truth. He is determined
+===
+create a character based on the following information:
+plot: ${plot}
+genre: ${genre}
+role: ${role}
+~~~`;
+}
---
+++ src/pages/tools/character-creator.tsx
@@ -0,0 +1,189 @@
+import { Card, Label, Select, Textarea } from 'flowbite-react';
+import React, { useContext, useState } from 'react';
+import { HiOutlineArrowPath, HiOutlineClipboard } from 'react-icons/hi2';
+
+import axiosApiInstance from '@/lib/updateIdToken';
+import useAuth from '@/hooks/useAuth';
+
+import Button from '@/components/buttons/Button';
+import Alert from '@/components/layout/Alert';
+import Layout from '@/components/layout/Layout';
+
+import { url } from '@/constant/url';
+import { AlertContext } from '@/context/AlertState';
+
+const genres = [
+  {
+    id: '1',
+    name: 'Drama',
+    value: 'drama',
+  },
+  {
+    id: '2',
+    name: 'Fantasy',
+    value: 'fantasy',
+  },
+  {
+    id: '3',
+    name: 'Mystery',
+    value: 'mystery',
+  },
+  {
+    id: '4',
+    name: 'Romance',
+    value: 'romance',
+  },
+  {
+    id: '5',
+    name: 'Sci-Fi',
+    value: 'sci-fi',
+  },
+];
+
+const roles = [
+  {
+    id: '1',
+    name: 'Protaganist',
+  },
+  {
+    id: '2',
+    name: 'Antagonist',
+  },
+  {
+    id: '3',
+    name: 'Secondary character',
+  },
+  {
+    id: '4',
+    name: 'Love interest',
+  },
+  {
+    id: '5',
+    name: 'Foil',
+  },
+  {
+    id: '6',
+    name: 'Confidant',
+  },
+  {
+    id: '7',
+    name: 'Tertiary character',
+  },
+];
+
+const CharacterCreator = () => {
+  const { user } = useAuth();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [formData, setFormData] = useState({
+    genre: 'Drama',
+    role: 'Protaganist',
+    plot: '',
+  });
+  const [character, setCharacter] = useState('');
+  const alertContext = useContext(AlertContext);
+  const onChange = (
+    e: React.ChangeEvent<HTMLSelectElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const generateCharacter = async () => {
+    setLoading(true);
+    const res = await axiosApiInstance.post(
+      `${url}/api/generate/character-creator`,
+      {
+        uid: user?.uid,
+        genre: formData.genre,
+        role: formData.role,
+        plot: formData.plot,
+      }
+    );
+
+    setCharacter(res.data.choices[0].text);
+
+    setLoading(false);
+  };
+
+  return (
+    <Layout title='Character Creator'>
+      <Alert />
+      <div className='mx-auto lg:max-w-7xl '>
+        <div className='mt-4 py-6'>
+          <div className='my-3'>
+            <p>Create a basic character profile for your story</p>
+          </div>
+          <div className='space-y-4'>
+            <div>
+              <Label htmlFor='genre' value='Select a genre' />
+              <Select color='bg-transparent' onChange={onChange} name='genre'>
+                {genres.map((genre) => {
+                  return (
+                    <option key={genre.id} id={genre.id}>
+                      {genre.name}
+                    </option>
+                  );
+                })}
+              </Select>
+            </div>
+            <div>
+              <Label htmlFor='role' value='Select a role' />
+              <Select color='bg-transparent' onChange={onChange} name='role'>
+                {roles.map((role) => {
+                  return (
+                    <option key={role.id} id={role.id}>
+                      {role.name}
+                    </option>
+                  );
+                })}
+              </Select>
+            </div>
+            <div>
+              <Label htmlFor='plot' value='Enter your plot' />
+              <Textarea
+                className='resize-none !bg-transparent'
+                name='plot'
+                rows={6}
+                onChange={onChange}
+              />
+            </div>
+            <div>
+              <Button onClick={generateCharacter} isLoading={loading}>
+                Generate outline{' '}
+                <span>
+                  <HiOutlineArrowPath className=' ml-2 h-4 w-4' />
+                </span>
+              </Button>
+            </div>
+          </div>
+          <div className='mt-6'>
+            {character ? (
+              <Card>
+                <div className='flex flex-row items-center justify-between'>
+                  {character ? (
+                    <p className='flex-grow whitespace-pre-line'>{character}</p>
+                  ) : null}
+                  <div className='px-1 '>
+                    <button
+                      onClick={() => {
+                        navigator.clipboard.writeText(character);
+                        alertContext.addAlert(
+                          'Copied to clipboard',
+                          'info',
+                          3000
+                        );
+                      }}
+                    >
+                      <HiOutlineClipboard className='h-5  w-5 hover:shadow-sm' />
+                    </button>
+                  </div>
+                </div>
+              </Card>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default CharacterCreator;
