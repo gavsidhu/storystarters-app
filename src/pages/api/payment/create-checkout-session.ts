e95c1777f2343743bd2a8d2464a17a3e534ab875
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-02-09 12:15:30 -0800

    feat: allowed promo codes for payments

--- src/pages/api/payment/create-checkout-session.ts
+++ src/pages/api/payment/create-checkout-session.ts
@@ -55,6 +55,7 @@ export default async function handler(
           },
         ],
         mode: 'payment',
+        allow_promotion_codes: true,
         success_url: `${url}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}?canceled=true`,
       });
